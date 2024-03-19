76963d96ccf28b04d7cdf3a50c26042e5f3bfbbd
Justine Macmillan (ASUS PC) <jlm611@york.ac.uk>
2024-03-19 15:33:22 +0000

    added l_max

--- NS+Seabass.R
+++ NS+Seabass.R
@@ -12,5 +12,51 @@
 
 # load the required libraries
   library(mizer) # where we will be doing the majority of our coding
+  library(tidyverse) # to analyse and plot data
+  library(rfishbase) # to extract fish data
+ 
+# choose species to include
+  # will base it on Blanchard et al., 2014's North Sea model 
+    sp_NS <- NS_species_params$species
+    
+  # we will include European Seabass following Townhill et al., 2023 which states that the NS will become more habitable for them
+    sp_NS <- c(sp_NS, "E. Seabass")
+    
+# Create a df to base to mizerParams on
+  # base df: has species names
+    sp_NS <- data.frame(species = sp_NS)
+    # add metadata which explains species choice in the species column
+      comment(sp_NS$species) <- "We use the species choice made by Blanchard et al., 2014 https://doi.org/10.1111/1365-2664.12238 and add Sebass as it is predicted that the NS will become more suitable for it by Townhill et al., 2023 https://doi.org/10.1111/faf.12773"
   
+  # add a column with the scientific names of each species
+      sp_NS$latin_name <- c("Sprattus sprattus", # Sprat
+                         "Ammodutyes marinus", # Sandeel
+                         "Trisopterus esmarkii", # Norway Pout
+                         "Clupea harengus", # Herring
+                         "Limanda limanda", # Common Dab
+                         "Merlangius merlangus", # Whiting
+                         "Solea solea", # Sole
+                         "Eutrigla gurnardus", # Gurnard
+                         "Pleuronectes platessa", # Plaice
+                         "Melanogrammus aeglefinus", # Haddock
+                         "Gadus morhua", # Cod
+                         "Pollachius virens", # Saithe
+                         "Dicentrarchus labrax") # European Seabass
+      comment(sp_NS$latin_name) <- "used the latin names provided by Blanchard et al., 2014 https://doi.org/10.1111/1365-2664.12238 and the latin name found in Fishbase page for European Seabass"
 
+  # add asymptotic size (w_inf). Asymptotic size is the maximum w_inf, not an average
+      # using rfishbase::species() to extract relevant information from Fishbase database
+          max_size_fishbase <- rfishbase::species(sp_NS[,2]) |>
+            select(latin_name = Species, l_max = Length)
+          
+      # combining the df to have one master df
+          max_size <- max_size_fishbase |>
+            left_join(select(sp_NS, species, latin_name),
+                      by = "latin_name")
+          max_size <- bind_rows(max_size, max_size_fishbase) |>
+            select(species, l_max)
+          max_size
+                          
+                         
+    
+  
